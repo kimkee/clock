7cd37710263c762821d11424070075db6ecc7dd2
김기현 <kimkee7@gmail.com>
2022-08-18 12:48:21 +0900

    digt

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-let hour = document.getElementById("hour");
-let mins = document.getElementById("mins");
-let secs = document.getElementById("secs");
-let ampm = document.getElementById("ampm");
+const hour = document.getElementById("hour");
+const mins = document.getElementById("mins");
+const secs = document.getElementById("secs");
+const ampm = document.getElementById("ampm");
 
-let hh = document.getElementById("hh");
-let mm = document.getElementById("mm");
-let ss = document.getElementById("ss");
+const tit = document.querySelector(".tit");
+const hh = document.getElementById("hh");
+const mm = document.getElementById("mm");
+const ss = document.getElementById("ss");
 
-let dothh = document.querySelector(".dot.hh");
-let dotmm = document.querySelector(".dot.mm");
-let dotss = document.querySelector(".dot.ss");
+const dothh = document.querySelector(".dot.hh");
+const dotmm = document.querySelector(".dot.mm");
+const dotss = document.querySelector(".dot.ss");
 
-setInterval(() => {
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
+const digt = n => n < 10 ? "0" + n : n;
+
+setInterval( e => {
+
+    const date = new Date();
+    const yy = digt(date.getFullYear() );
+    const mo = digt(date.getMonth()+1 );
+    const dy = digt(date.getDay() );
+
+    let h = digt(date.getHours());
+    let m = digt(date.getMinutes());
+    let s = digt(date.getSeconds());
     let am = h >= 12 ? "PM" : "AM";
-
     h > 12 ? h = h - 12 : null;
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
     
+    tit.innerHTML = yy +"."+ mo +"."+ dy;
     hour.innerHTML = h + "<i>Hours</i>";
     mins.innerHTML = m + "<i>Minutes</i>";
     secs.innerHTML = s + "<i>Seconds</i>";
     ampm.innerHTML = am;
 
-    hh.style.strokeDashoffset = 220 - (220 * h) / 12;
-    mm.style.strokeDashoffset = 220 - (220 * m) / 60;
-    ss.style.strokeDashoffset = 220 - (220 * s) / 60;
+    const round = hh.getTotalLength();
+
+    hh.style.strokeDashoffset = round - (round * h) / 12;
+    mm.style.strokeDashoffset = round - (round * m) / 60;
+    ss.style.strokeDashoffset = round - (round * s) / 60;
 
     dothh.style.transform = 'rotate('+ h * 360 / 12 +'deg)';
     dotmm.style.transform = 'rotate('+ m * 360 / 60 +'deg)';
     dotss.style.transform = 'rotate('+ s * 360 / 60 +'deg)';
-
-});
+    // console.log(round);
+},500);
